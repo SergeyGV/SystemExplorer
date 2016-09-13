1d4b6cec641e02f31aaaf693d2b6a226815dda35
SergeyGV <sergey.gayvoronskiy@mail.utoronto.ca>
2016-09-13 12:08:21 -0400

    Refactored running file

--- warp.sh
+++ warp.sh
@@ -1,34 +1,10 @@
 #!bin/bash
 gcc -Wall explorer.c counter.c
-flag_p=""
-flag_f=""
-path=""
-while getopts 'pfd:' flag;
-do
-	case $flag in
-		p) flag_p="p" ;;
-		f) flag_f="f" ;;
-		d) 
-		if [ "$path" != "" ];
-		then
-			echo "usage: . [-d DIRPATH] [-f] [-p]"
-			exit
-		else
-			path=$OPTARG
-		fi ;;		
-		\?) 
-			echo "usage: . [-d DIRPATH] [-f] [-p]"
-			exit ;;
-	esac
-done
-path=`./a.out $path $p$f`
-if [ $? -ne 0 ];
+path=`./a.out "$@"`
+if [ $? -eq 0 ];
 then
-	echo "Something went wrong. Did you put in a valid directory?"
- 
-elif [ "$flag_p" = "p" ];
+	cd $path
+elif [ "$path" != "" ];
 then
 	echo $path
-else
-	cd $path
 fi
